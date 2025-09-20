d38b329ec6c7bb55eae8608fb56e601a491e55ff
Wei (Scarlett) Xu <88093404+lettiehsu@users.noreply.github.com>
2025-09-20 18:42:26 -0500

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,6 @@
-bind = "0.0.0.0:10000"  # Render forwards to PORT, but Procfile / env will override
+bind = "0.0.0.0:10000"
 workers = 2
-threads = 2
+threads = 4
 timeout = 120
-graceful_timeout = 30
-keepalive = 5
+accesslog = "-"
+errorlog = "-"
